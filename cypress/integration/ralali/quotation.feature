8666c2bb5fd706ce55902fef0cb7be82ac5834c6
damar <damar.aji@ralali.com>
2020-03-22 11:59:52 +0700

    update quotation test

--- cypress/integration/ralali/quotation.feature
+++ cypress/integration/ralali/quotation.feature
@@ -9,5 +9,6 @@ Feature: Ralali Website - Request Quotation
     Then I click the button request quotation
     Then I fill in name field with "Sinta"
     Then I fill in email field with "sinta@mail.com"
+    Then I choose country code with "+66"
     Then I fill in phone field with "8577123456"
     Then I click the button next
--- cypress/integration/ralali/quotation/quotation.js
+++ cypress/integration/ralali/quotation/quotation.js
@@ -3,6 +3,7 @@ import { Then } from 'cypress-cucumber-preprocessor/steps'
 const buttonQuotation = 'button[id="btn-post-buying-request"'
 const nameField = 'input[name="name"]'
 const emailField = 'input[name="email"]'
+const countryCodeOption = 'select[ng-model="form.country_code"]'
 const phoneField = 'input[name="phone"]'
 const buttonNext = 'button[ng-click="buyingReq.saveAccountInfo();"]' //not best practices
 
@@ -13,16 +14,25 @@ Then(`I click the button request quotation`, () => {
 Then(`I fill in name field with {string}`, name => {
   cy.get(nameField)
     .type(name)
+    .should('have.value', name)
 })
 
 Then(`I fill in email field with {string}`, email => {
   cy.get(emailField)
     .type(email)
+    .should('have.value', email)
+})
+
+Then(`I choose country code with {string}`, countryCode => {
+  cy.get(countryCodeOption)
+    .select(countryCode)
+    .should('have.value', countryCode)
 })
 
 Then(`I fill in phone field with {string}`, phone => {
   cy.get(phoneField)
     .type(phone)
+    .should('have.value', phone)
 })
 
 Then(`I click the button next`, () => {
